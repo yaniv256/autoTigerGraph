7aca491758fa71fe0edbfb47e8d0b586b61050c0
Yaniv Ben-Ami <yaniv256@gmail.com>
2020-07-02 23:05:52 +0000

    forgot to save notebook

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="autoTigerGraph",
-    version="0.0.1",
+    version="0.0.2",
     author="Yaniv Ben-Ami",
     author_email="yaniv256@gmail.com",
     description="A package for automaticaly generating TigerGraph schema and upserting data",
